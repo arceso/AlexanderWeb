bb392ffac0d99815f5fcd02c176cb3fafe7d7f2c
Arceso <arcesone@gmail.com>
2018-06-02 20:25:04 +0200

    Finished all setups
    With love,
    For you
    From me.
    ♥

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass        = require('gulp-sass');
 const rename      = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 
+const htmlPath    = './Deploy/*.html';
 const sassPath    = './Code/SASS/**/*.scss';
 const sassStyle   = './Code/SASS/08-Common.scss';
 
@@ -10,14 +11,19 @@ const deployPath = './Deploy';
 
 gulp.task('default', ['browserSync'], function() {
   gulp.watch(sassPath, ['sass']);
+  gulp.watch(htmlPath, ['html']);
 });
 
 gulp.task('sass', function() {
-  gulp.src(sassPath)
+  gulp.src(sassStyle)
     .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
     .pipe(rename('style.css'))
     .pipe(gulp.dest(deployPath))
     .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('html', function() {
+  gulp.src(htmlPath).pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('browserSync', function() {
